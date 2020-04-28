eb2b74ebafc131bdcc64373051ac9eb19d3832e0
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-27 21:07:57 -0400

    getting guitar info from db

--- src/GuitarContainer/index.js
+++ src/GuitarContainer/index.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 export default class GuitarContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { guitars: [] }
+  }
+
+  componentDidMount() {
+    this.getGuitar();
+  }
+  getGuitar = async () => {
+    try {
+      const url = process.env.REACT_APP_API_URL + '/';
+      console.log(url)
+      const res = await fetch(url);
+      const json = await res.json();
+      console.log('here is the json')
+      console.log(json);
+
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
