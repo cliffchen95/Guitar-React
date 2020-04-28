a442480152df472d12d2d4d42487a23762aa4932
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-27 20:50:50 -0400

    render guitar continaer

---
+++ src/GuitarContainer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class GuitarContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        Guitar COntainer 
+      </React.Fragment>
+    )
+  }
+}
