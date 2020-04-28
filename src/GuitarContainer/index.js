3d5052ee08663f12cf210eb26ad2bca773662254
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-27 22:34:13 -0400

    guitar form submiting with information of the new guitar

--- src/GuitarContainer/index.js
+++ src/GuitarContainer/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import GuitarList from '../GuitarList';
+import GuitarNewForm from '../GuitarNewForm';
 
 export default class GuitarContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { guitars: [] }
+    this.state = { 
+      guitars: [],
+      newGuitar: false
+    }
   }
 
   componentDidMount() {
     this.getGuitar();
   }
+  // get guitar list
   getGuitar = async () => {
     try {
       const url = process.env.REACT_APP_API_URL + '/';
@@ -22,12 +27,20 @@ export default class GuitarContainer extends Component {
     }
   }
 
+  // submit new guitar info
+  addGuitar = (guitar) => {
+    console.log(guitar)
+  }
   render() {
     console.log("here is states in container")
     console.log(this.state)
     return (
       <React.Fragment>
         <h2>Guitars!!</h2>
+        { this.state.newGuitar 
+          ? <GuitarNewForm addGuitar={this.addGuitar} /> 
+          : <button onClick={ () => this.setState({ newGuitar: true }) }>New Guitar</button>
+        }
         <GuitarList guitars={this.state.guitars} />
       </React.Fragment>
     )
---
+++ src/GuitarNewForm/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+
+export default class GuitarNewForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      price: "",
+      isElectric: false
+    }
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+  toggle = () => this.setState((prevState) => ({ isElectric: !prevState.isElectric }))
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addGuitar(this.state);
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field>
+          <label>Name:</label>
+          <input
+            name='name' 
+            placeholder='Name'
+            value={this.state.name}
+            onChange={this.onChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Price:</label>
+          <input
+            name='price'
+            placeholder='Price'
+            value={this.state.price}
+            onChange={this.onChange} 
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            name='isElectric' 
+            label='is electric'
+            onChange = {this.toggle}
+          />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    )
+  }
+}
